902d6cb77cd05ea1f1f50e34ebd777f28d51bf69
haruka-g <mortndpgj@gmail.com>
2019-11-12 21:39:47 +0900

    assert.equalをassert.strictEqualに変更

--- test/src/Counter.spec.js
+++ test/src/Counter.spec.js
@@ -5,39 +5,39 @@ describe('Counterクラスのテスト', () => {
    
         it('_countプロパティの有無を確認', () =>{
             const counter = new Counter();
-            assert.equal(counter.hasOwnProperty('_count'), true);
+            assert.strictEqual(counter.hasOwnProperty('_count'), true);
         });
 
         it('_countプロパティの初期値が0であることを確認', () =>{
             const counter = new Counter();
-            assert.equal(counter._count, 0);
+            assert.strictEqual(counter._count, 0);
         });
 
       
         it('Counterクラスはincrementメソッドを持つ', () =>{
             const counter = new Counter();
-            assert.equal(typeof counter.increment === 'function', true);
+            assert.strictEqual(typeof counter.increment === 'function', true);
         });
    
         it('incrementメソッドを実行すると_countプロパティの値は１増える', () =>{
             const counter = new Counter();
 
-            assert.equal(counter._count, 0);
+            assert.strictEqual(counter._count, 0);
             counter.increment();
-            assert.equal(counter._count, 1);
+            assert.strictEqual(counter._count, 1);
         });
    
         it('Counterクラスはdecrementメソッドを持つ', () =>{
             const counter = new Counter();
-            assert.equal(typeof counter.decrement === 'function', true);
+            assert.strictEqual(typeof counter.decrement === 'function', true);
         });
 
         it('decrementメソッドを実行すると_countプロパティの値は１減る', () =>{
             const counter = new Counter();
 
-            assert.equal(counter._count, 0);
+            assert.strictEqual(counter._count, 0);
             counter.decrement();
-            assert.equal(counter._count, -1);
+            assert.strictEqual(counter._count, -1);
         });
   
 
